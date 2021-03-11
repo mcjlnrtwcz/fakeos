99ac5cbe065caa6c9cf80202d65d313a5fdee364
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2021-03-11 23:00:09 +0100

    Move app launcher to the right side

--- src/components/AppLauncher/AppLauncher.js
+++ src/components/AppLauncher/AppLauncher.js
@@ -15,13 +15,30 @@ export default function AppLauncher() {
         <img src={searchIcon} class={styles['search-icon']} />
       </div>
       <div class={styles.apps}>
+        <AppGroup name="Favorites" customClass={styles.favorites}>
+          <App icon={calculatorIcon}>Calculator</App>
+          <App icon={terminalIcon}>Terminal</App>
+        </AppGroup>
+        <AppGroup name="Utilities">
+          <App icon={calculatorIcon}>Calculator</App>
+        </AppGroup>
+        <AppGroup name="System">
+          <App icon={terminalIcon}>Terminal</App>
+          <App icon={helpIcon}>Help</App>
+        </AppGroup>
         {/* TODO: apps should be in some app repository with respective icons */}
-        {/* TODO: group apps in categories */}
-        <App icon={calculatorIcon}>Calculator</App>
-        <App icon={terminalIcon}>Terminal</App>
-        <App icon={helpIcon}>Help</App>
       </div>
       {/* TODO: place clock at the bottom? */}
+    </div>
+  );
+}
+
+function AppGroup({ name, customClass, children }) {
+  return (
+    <div class={customClass}>
+      <span class={styles['app-category']}>{name}</span>
+      {/* TODO: groups should be collapsible */}
+      {children}
     </div>
   );
 }
